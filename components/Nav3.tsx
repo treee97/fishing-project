dcf22cf39394b741d7cf077a5e35f8d408406e70
Luiggi <luduve97@gmail.com>
2023-09-19 12:12:44 +0200

    navbar flickering when no login

--- components/Nav3.tsx
+++ components/Nav3.tsx
@@ -17,148 +17,166 @@ import ThemeToggleButton from "./themeToggleBtn";
 //my hooks
 
 const Navbar = () => {
-  const { data: session } = useSession();
-  const [providers, setProviders] = useState<null | unknown>(null);
-  const router = useRouter();
-  const { resolvedTheme, setTheme } = useTheme();
+	const { data: session } = useSession();
+	const [providers, setProviders] = useState<null | unknown>(null);
+	const router = useRouter();
+	const { resolvedTheme, setTheme } = useTheme();
+	const [prevScrollPos, setPrevScrollPos] = useState<number>(0);
+	const [visible, setVisible] = useState<boolean>(true);
 
-  useEffect(() => {
-    (async () => {
-      const res = await getProviders();
-      setProviders(res);
-    })();
+	useEffect(() => {
+		(async () => {
+			const res = await getProviders();
+			setProviders(res);
+		})();
 
-    //if the user logs off we return the session provider state to null
-    if (!session) {
-      setProviders(null);
-    }
-  }, [session]);
-  //
-  return (
-    // fixed top-0 left-0 z-50
-    <nav className="fixed bg-black bg-opacity-20 top-0 left-0 z-50 w-full h-20 flex items-center justify-around ">
-      {/* primera columna: logo */}
-      <Link href="/">
-        <Image width={44} height={80} src={logo_fish} alt="fish logo" />
-      </Link>
+		//if the user logs off we return the session provider state to null
+		if (!session) {
+			setProviders(null);
+		}
 
-      {session?.user ? (
-        <>
-          {/*segunda columna: links centrados entre logo y signin */}
-          <div className="hidden md:flex items-center justify-center gap-4">
-            <div className="flex items-center space-x-4">
-              <Link
-                href="/"
-                className="text-light-text dark:text-dark-text text-xl font-normal"
-              >
-                Home
-              </Link>
-              <Link
-                href="marketplace"
-                className="text-light-text dark:text-dark-text text-xl font-normal"
-              >
-                Marketplace
-              </Link>
-              <OpenGameButton session={session} />
-              <Link
-                href="inventory"
-                className="text-light-text dark:text-dark-text text-xl font-normal"
-              >
-                Inventory
-              </Link>
-            </div>
-          </div>
-          {/* tercera columna: sign in */}
-          <div className="hidden md:flex items-center justify-between gap-4">
-            <ThemeToggleButton />
-            <div className="flex items-center justify-around">
-              <Image
-                src={session?.user.image || "/assets/images/fish.svg"}
-                width={37}
-                height={37}
-                className="rounded-full"
-                alt="profile"
-              />
-              <button
-                type="button"
-                className="text-light-text dark:text-dark-text text-xl font-normal"
-                onClick={async () => {
-                  await signOut();
-                  router.push("/");
-                }}
-              >
-                Sign
-                <br />
-                Out
-              </button>
-            </div>
-          </div>
-          {/* mobile menu  when logged*/}
-          {/* cuando el elem es >= 768px se esconde, si es mas
-          pequeño entonces se vizualiza */}
-          <div className="md:hidden">
-            <OpenGameButton session={session} />
-          </div>
-          <NavMobile
-            session={session}
-            provider={providers}
-            className="md:hidden"
-          />
-        </>
-      ) : (
-        <>
-          <div className="hidden md:flex items-center justify-center gap-4">
-            <OpenGameButton session={session} />
-          </div>
+		const handleScroll = () => {
+			const currentScrollPos = window.scrollY;
+			if (prevScrollPos > currentScrollPos) {
+				setVisible(true);
+			} else {
+				setVisible(false);
+			}
+			setPrevScrollPos(currentScrollPos);
+		};
 
-          {providers &&
-            Object.values(providers).map((provider, index) => (
-              <div
-                className="hidden md:flex align-center justify-center text-light-text dark:text-dark-text"
-                key={index}
-              >
-                <div className="flex items-center justify-center gap-4">
-                  <ThemeToggleButton />
-                  <Image
-                    src={
-                      resolvedTheme === "dark"
-                        ? "./assets/icons/icon-user-light.svg"
-                        : "./assets/icons/icon-user.svg"
-                    }
-                    alt="user icon"
-                    width="0"
-                    height="0"
-                    className="w-full h-auto"
-                  />
-                  <button
-                    type="button"
-                    key={provider.name}
-                    onClick={() => {
-                      signIn(provider.id);
-                    }}
-                    className="text-xl font-normal"
-                  >
-                    Sign
-                    <br />
-                    in
-                  </button>
-                </div>
-              </div>
-            ))}
+		window.addEventListener("scroll", handleScroll);
+	}, [session, prevScrollPos]);
+	//
+	return (
+		// fixed top-0 left-0 z-50
+		<nav
+			className={`fixed bg-black bg-opacity-20 left-0 z-50 w-full h-20 flex items-center justify-around transition-all duration-300 ease-in-out ${
+				visible ? "top-0 " : "-top-20"
+			}`}
+		>
+			{/* primera columna: logo */}
+			<Link href="/">
+				<Image width={44} height={80} src={logo_fish} alt="fish logo" />
+			</Link>
 
-          <div className="md:hidden">
-            <OpenGameButton session={session} />
-          </div>
+			{!session?.user ? (
+				<>
+					<div className="hidden md:flex items-center justify-center gap-4">
+						<OpenGameButton session={session} />
+					</div>
 
-          <NavMobile
-            session={session}
-            provider={providers}
-            className="md:hidden"
-          />
-        </>
-      )}
-    </nav>
-  );
+					{providers &&
+						Object.values(providers).map((provider, index) => (
+							<div
+								className="hidden md:flex align-center justify-center text-light-text dark:text-dark-text"
+								key={index}
+							>
+								<div className="flex items-center justify-center gap-4">
+									<ThemeToggleButton />
+									<Image
+										src={
+											resolvedTheme === "dark"
+												? "./assets/icons/icon-user-light.svg"
+												: "./assets/icons/icon-user.svg"
+										}
+										alt="user icon"
+										width="0"
+										height="0"
+										className="w-full h-auto"
+									/>
+									<button
+										type="button"
+										key={provider.name}
+										onClick={() => {
+											signIn(provider.id);
+										}}
+										className="text-xl font-normal"
+									>
+										Sign
+										<br />
+										in
+									</button>
+								</div>
+							</div>
+						))}
+
+					<div className="md:hidden">
+						<OpenGameButton session={session} />
+					</div>
+
+					<NavMobile
+						session={session}
+						provider={providers}
+						className="md:hidden"
+					/>
+				</>
+			) : (
+				<>
+					{/*segunda columna: links centrados entre logo y signin */}
+					<div className="hidden md:flex items-center justify-center gap-4">
+						<div className="flex items-center space-x-4">
+							<Link
+								href="/"
+								className="text-light-text dark:text-dark-text text-xl font-normal"
+							>
+								Home
+							</Link>
+							<Link
+								href="marketplace"
+								className="text-light-text dark:text-dark-text text-xl font-normal"
+							>
+								Marketplace
+							</Link>
+							<OpenGameButton session={session} />
+							<Link
+								href="inventory"
+								className="text-light-text dark:text-dark-text text-xl font-normal"
+							>
+								Inventory
+							</Link>
+						</div>
+					</div>
+					{/* tercera columna: sign in */}
+					<div className="hidden md:flex items-center justify-between gap-4">
+						<ThemeToggleButton />
+						<div className="flex items-center justify-around">
+							<Image
+								src={session?.user.image || "/assets/images/fish.svg"}
+								width={37}
+								height={37}
+								className="rounded-full"
+								alt="profile"
+							/>
+							<button
+								type="button"
+								className="text-light-text dark:text-dark-text text-xl font-normal"
+								onClick={async () => {
+									await signOut();
+									router.push("/");
+								}}
+							>
+								Sign
+								<br />
+								Out
+							</button>
+						</div>
+					</div>
+					{/* mobile menu  when logged*/}
+					{/* cuando el elem es >= 768px se esconde, si es mas
+        pequeño entonces se vizualiza */}
+					<div className="md:hidden">
+						<OpenGameButton session={session} />
+					</div>
+					<NavMobile
+						session={session}
+						provider={providers}
+						className="md:hidden"
+					/>
+				</>
+			)}
+		</nav>
+	);
 };
 
 export default Navbar;
--- components/NavMobile.tsx
+++ components/NavMobile.tsx
@@ -15,140 +15,140 @@ import waveHamburgerLight from "public/assets/icons/wave_hamburger.svg";
 import ThemeToggleButton from "./themeToggleBtn";
 
 export interface IMenuProps {
-  session: Session | null;
-  provider: any;
-  className: string;
+	session: Session | null;
+	provider: any;
+	className: string;
 }
 
 const NavMobile = ({ session, provider, className }: IMenuProps) => {
-  const [showMenu, setShowMenu] = useState<boolean>(false);
-  const { resolvedTheme, setTheme } = useTheme();
+	const [showMenu, setShowMenu] = useState<boolean>(false);
+	const { resolvedTheme, setTheme } = useTheme();
 
-  useEffect(() => {
-    if (showMenu) {
-      document.body.classList.add("disable-scroll");
-    } else {
-      document.body.classList.remove("disable-scroll");
-    }
+	useEffect(() => {
+		if (showMenu) {
+			document.body.classList.add("disable-scroll");
+		} else {
+			document.body.classList.remove("disable-scroll");
+		}
 
-    // Cleanup the effect
-    return () => {
-      document.body.classList.remove("disable-scroll");
-    };
-  }, [showMenu]);
+		// Cleanup the effect
+		return () => {
+			document.body.classList.remove("disable-scroll");
+		};
+	}, [showMenu]);
 
-  const toggleMenu = () => {
-    setShowMenu((prev) => !prev);
+	const toggleMenu = () => {
+		setShowMenu((prev) => !prev);
 
-    // Toggle the scroll lock on body
-    if (!showMenu) {
-      document.body.classList.add("disable-scroll");
-    } else {
-      document.body.classList.remove("disable-scroll");
-    }
-  };
+		// Toggle the scroll lock on body
+		if (!showMenu) {
+			document.body.classList.add("disable-scroll");
+		} else {
+			document.body.classList.remove("disable-scroll");
+		}
+	};
 
-  return (
-    <>
-      <div className={className}>
-        <div className="flex items-center justify-between">
-          <button type="button" onClick={toggleMenu} className="block">
-            <Image
-              src={
-                resolvedTheme === "dark"
-                  ? waveHamburgerDark
-                  : waveHamburgerLight
-              }
-              alt="Menu"
-              width="0"
-              height="0"
-              className="w-full h-auto"
-            />
-          </button>
-        </div>
-        {showMenu && (
-          <>
-            {session?.user ? (
-              <div className="flex flex-col fixed top-0 right-0 items-center justify-center w-4/5 h-screen gap-7 bg-dark-background dark:bg-light-background text-dark-text dark:text-light-text">
-                <div>
-                  <CgClose
-                    className="cursor-pointer w-8 h-8 absolute top-8 right-8"
-                    onClick={() => setShowMenu(false)}
-                  />
-                </div>
-                <Link
-                  className=" text-xl font-normal"
-                  href="/"
-                  onClick={() => setShowMenu(false)}
-                >
-                  Home
-                </Link>
-                <Link
-                  className="text-xl font-normal"
-                  href="marketplace"
-                  onClick={() => setShowMenu(false)}
-                >
-                  Marketplace
-                </Link>
-                <Link
-                  className=" text-xl font-normal"
-                  href="inventory"
-                  onClick={() => setShowMenu(false)}
-                >
-                  Inventory
-                </Link>
-                <button
-                  type="button"
-                  className="text-xl font-normal"
-                  onClick={() => {
-                    signOut();
-                    setShowMenu(false);
-                  }}
-                >
-                  Sign Out
-                </button>
-                <div className="absolute bottom-4 right-4">
-                  <ThemeToggleButton />
-                </div>
-              </div>
-            ) : (
-              <>
-                <div className="flex flex-col fixed top-0 right-0 items-center justify-center w-4/5 h-screen gap-7 bg-dark-background dark:bg-light-background text-dark-text dark:text-light-text">
-                  <div>
-                    <CgClose
-                      className="cursor-pointer w-8 h-8 absolute top-8 right-8"
-                      onClick={() => setShowMenu(false)}
-                    />
-                  </div>
-                  <Link
-                    className="text-xl font-normal"
-                    href="/"
-                    onClick={() => setShowMenu(false)}
-                  >
-                    Home
-                  </Link>
-                  {provider &&
-                    Object.values(provider).map((provider: any) => (
-                      <button
-                        type="button"
-                        key={provider.name}
-                        onClick={() => {
-                          signIn(provider.id);
-                          setShowMenu(false);
-                        }}
-                        className="text-xl font-normal"
-                      >
-                        Sign in
-                      </button>
-                    ))}
-                </div>
-              </>
-            )}
-          </>
-        )}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className={className}>
+				<div className="flex items-center justify-between">
+					<button type="button" onClick={toggleMenu} className="block">
+						<Image
+							src={
+								resolvedTheme === "dark"
+									? waveHamburgerDark
+									: waveHamburgerLight
+							}
+							alt="Menu"
+							width="0"
+							height="0"
+							className="w-full h-auto"
+						/>
+					</button>
+				</div>
+				{showMenu && (
+					<>
+						{session?.user ? (
+							<div className="flex flex-col fixed top-0 right-0 items-center justify-center w-4/5 h-screen gap-7 bg-dark-background dark:bg-light-background text-dark-text dark:text-light-text">
+								<div>
+									<CgClose
+										className="cursor-pointer w-8 h-8 absolute top-8 right-8"
+										onClick={() => setShowMenu(false)}
+									/>
+								</div>
+								<Link
+									className=" text-xl font-normal"
+									href="/"
+									onClick={() => setShowMenu(false)}
+								>
+									Home
+								</Link>
+								<Link
+									className="text-xl font-normal"
+									href="marketplace"
+									onClick={() => setShowMenu(false)}
+								>
+									Marketplace
+								</Link>
+								<Link
+									className=" text-xl font-normal"
+									href="inventory"
+									onClick={() => setShowMenu(false)}
+								>
+									Inventory
+								</Link>
+								<button
+									type="button"
+									className="text-xl font-normal"
+									onClick={() => {
+										signOut();
+										setShowMenu(false);
+									}}
+								>
+									Sign Out
+								</button>
+								<div className="absolute bottom-4 right-4">
+									<ThemeToggleButton />
+								</div>
+							</div>
+						) : (
+							<>
+								<div className="flex flex-col fixed top-0 right-0 items-center justify-center w-4/5 h-screen gap-7 bg-dark-background dark:bg-light-background text-dark-text dark:text-light-text">
+									<div>
+										<CgClose
+											className="cursor-pointer w-8 h-8 absolute top-8 right-8"
+											onClick={() => setShowMenu(false)}
+										/>
+									</div>
+									<Link
+										className="text-xl font-normal"
+										href="/"
+										onClick={() => setShowMenu(false)}
+									>
+										Home
+									</Link>
+									{provider &&
+										Object.values(provider).map((provider: any) => (
+											<button
+												type="button"
+												key={provider.name}
+												onClick={() => {
+													signIn(provider.id);
+													setShowMenu(false);
+												}}
+												className="text-xl font-normal"
+											>
+												Sign in
+											</button>
+										))}
+								</div>
+							</>
+						)}
+					</>
+				)}
+			</div>
+		</>
+	);
 };
 
 export default NavMobile;
