308ca4c7b461fb47c9cfd576d7901bbd7e59a341
Luiggi <luduve97@gmail.com>
2023-09-20 12:39:49 +0200

    reverted to 13.4.9 - force-dynamic to getmarket

--- components/MarketPlace/MarketTable.tsx
+++ components/MarketPlace/MarketTable.tsx
@@ -4,43 +4,45 @@ import MarketItemRow from "./MarketItemRow";
 // import ClientPusher from "@/services/ClientPusher";
 
 const MarketTable = () => {
-  const [marketData, setMarketData] = useState([]);
+	const [marketData, setMarketData] = useState([]);
 
-  useEffect(() => {
-    // Fetch data from your API endpoint
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/api/getmarketplace");
-        console.log("response is being called!");
+	useEffect(() => {
+		// Fetch data from your API endpoint
+		const fetchData = async () => {
+			try {
+				const response = await fetch("/api/getmarketplace", {
+					cache: "no-store",
+				});
+				console.log("response is being called!");
 
-        const data = await response.json();
-        setMarketData(data);
-      } catch (error) {
-        console.error("Error fetching marketplace data:", error);
-      }
-    };
+				const data = await response.json();
+				setMarketData(data);
+			} catch (error) {
+				console.error("Error fetching marketplace data:", error);
+			}
+		};
 
-    fetchData();
-  }, []);
+		fetchData();
+	}, []);
 
-  return (
-    <div className="section flex justify-center items-center">
-      <table className="overflow-hidden text-lg table-fixed border border-slate-500 text-light-text dark:text-light-text w-4/5 rounded-lg">
-        <tbody>
-          <tr className="text-dark-text dark:text-light-text bg-dark-background dark:bg-light-background">
-            <th className="border border-slate-600">Item</th>
-            <th className="border border-slate-600">Rarity</th>
-            <th className="border border-slate-600">Quantity</th>
-            <th className="border border-slate-600">Price/u.</th>
-            <th className="border border-slate-600">Purchase</th>
-          </tr>
-          {marketData.map((item, index) => (
-            <MarketItemRow key={index} item={item} />
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+	return (
+		<div className="section flex justify-center items-center">
+			<table className="overflow-hidden text-lg table-fixed border border-slate-500 text-light-text dark:text-light-text w-4/5 rounded-lg">
+				<tbody>
+					<tr className="text-dark-text dark:text-light-text bg-dark-background dark:bg-light-background">
+						<th className="border border-slate-600">Item</th>
+						<th className="border border-slate-600">Rarity</th>
+						<th className="border border-slate-600">Quantity</th>
+						<th className="border border-slate-600">Price/u.</th>
+						<th className="border border-slate-600">Purchase</th>
+					</tr>
+					{marketData.map((item, index) => (
+						<MarketItemRow key={index} item={item} />
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 };
 
 export default MarketTable;
