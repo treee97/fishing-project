0ec22d4be4283d72d5ee5bdfef0a7b140f5d62d6
Luiggi <luduve97@gmail.com>
2023-09-26 12:41:29 +0200

    added analytics 4f

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,45 @@
 import "@/styles/globals.css";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 // By using an underscore (_) to name the imported variable, you are effectively telling the linter and compiler that you are intentionally not using the imported value and to ignore any related errors.
 
 //fonts
 import { SilkScreen, KongText } from "@/fonts/fonts";
 //my components
 import Navbar from "@/components/Nav3";
-import Footer from "@/components/Footer/Footer";
 import Provider from "@/components/Provider";
 //next-auth
 import { Session } from "next-auth";
 
 export const metadata: Metadata = {
-  title: "Fishy RPG",
-  description: "A fishing game",
+	title: "Fishy RPG",
+	description: "A fishing game",
 };
 
 export default function RootLayout({
-  children,
-  session,
+	children,
+	session,
 }: {
-  children: React.ReactNode;
-  session?: Session | undefined;
+	children: React.ReactNode;
+	session?: Session | undefined;
 }) {
-  return (
-    <html
-      lang="en"
-      className={`${KongText.variable} ${SilkScreen.variable} font-silkscreen light`}
-      style={{ colorScheme: "light" }}
-    >
-      <body>
-        <Provider session={session}>
-          <div className="main" />
-          <main className="app">
-            <Navbar />
-            {children}
-          </main>
-          {/* <Footer /> */}
-        </Provider>
-      </body>
-    </html>
-  );
+	return (
+		<html
+			lang="en"
+			className={`${KongText.variable} ${SilkScreen.variable} font-silkscreen light`}
+			style={{ colorScheme: "light" }}
+		>
+			<body>
+				<Provider session={session}>
+					<div className="main" />
+					<main className="app">
+						<Navbar />
+						{children}
+						<Analytics />
+					</main>
+					{/* <Footer /> */}
+				</Provider>
+			</body>
+		</html>
+	);
 }
