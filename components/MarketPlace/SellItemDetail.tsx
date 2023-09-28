42adaf371219daf9641f0775382746fe0219a4d8
Luiggi <luduve97@gmail.com>
2023-09-28 09:51:42 +0200

    added css to selltable

--- components/MarketPlace/SellItemDetail.tsx
+++ components/MarketPlace/SellItemDetail.tsx
@@ -9,121 +9,124 @@ import { useSession } from "next-auth/react";
 //my-components
 
 const SellItemDetail = ({ selectedItem, onSellItem }: any) => {
-  // console.log("this is selectedItem inside itemdetail =>", selectedItem);
-  const { data: session } = useSession();
-  const [quantity, setQuantity] = useState<number>(1);
-  const [price, setPrice] = useState<number>(selectedItem?.price ?? 1);
+	// console.log("this is selectedItem inside itemdetail =>", selectedItem);
+	const { data: session } = useSession();
+	const [quantity, setQuantity] = useState<number>(1);
+	const [price, setPrice] = useState<number>(selectedItem?.price ?? 1);
 
-  useEffect(() => {
-    // Update state when a new item is selected
-    setQuantity(selectedItem?.quantity || 1);
-    setPrice(selectedItem?.price || 1);
-  }, [selectedItem]);
+	useEffect(() => {
+		// Update state when a new item is selected
+		setQuantity(selectedItem?.quantity || 1);
+		setPrice(selectedItem?.price || 1);
+	}, [selectedItem]);
 
-  const onQuantityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newQuantity = Number(e.target.value);
-    const maxQuantity = selectedItem?.quantity || 1;
+	const onQuantityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const newQuantity = Number(e.target.value);
+		const maxQuantity = selectedItem?.quantity || 1;
 
-    // Ensure newQuantity is within the valid range (1 to maxQuantity)
-    if (newQuantity >= 1) {
-      setQuantity(newQuantity <= maxQuantity ? newQuantity : maxQuantity);
-      // si la nueva cantidad sigue siendo mas pequeña que el total entonces np. pero si se sobrepasa entonces automaticamente cambia a la cantidad total en nuestro inventario.
-    }
-  };
+		// Ensure newQuantity is within the valid range (1 to maxQuantity)
+		if (newQuantity >= 1) {
+			setQuantity(newQuantity <= maxQuantity ? newQuantity : maxQuantity);
+			// si la nueva cantidad sigue siendo mas pequeña que el total entonces np. pero si se sobrepasa entonces automaticamente cambia a la cantidad total en nuestro inventario.
+		}
+	};
 
-  const onPriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newPrice = Number(e.target.value);
-    if (newPrice >= 1) {
-      setPrice(newPrice);
-    }
-  };
+	const onPriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const newPrice = Number(e.target.value);
+		if (newPrice >= 1) {
+			setPrice(newPrice);
+		}
+	};
 
-  const handleSell = async (e: React.FormEvent) => {
-    e.preventDefault();
+	const handleSell = async (e: React.FormEvent) => {
+		e.preventDefault();
 
-    try {
-      const sellData = {
-        ...selectedItem,
-        quantity,
-        price,
-        userId: session?.user?.id,
-      };
-      // console.log("the sellData in itemDetail.tsx", sellData);
+		try {
+			const sellData = {
+				...selectedItem,
+				quantity,
+				price,
+				userId: session?.user?.id,
+			};
+			// console.log("the sellData in itemDetail.tsx", sellData);
 
-      // Make a POST request to your marketplace API
-      const response = await fetch("/api/sellitem", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(sellData),
-      });
+			// Make a POST request to your marketplace API
+			const response = await fetch("/api/sellitem", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(sellData),
+			});
 
-      // Handle the response as needed (e.g., show a success message, update inventory, etc.)
-      if (response.status === 201) {
-        onSellItem("The Item has been listed to the marketplace");
-      } else if (response.status !== 200) {
-        onSellItem("Error at listing the message");
-      }
-      // Update the user's inventory locally by reducing the quantity
-    } catch (error) {
-      console.error("Error selling item:", error);
-    }
-  };
+			// Handle the response as needed (e.g., show a success message, update inventory, etc.)
+			if (response.status === 201) {
+				onSellItem("The Item has been listed to the marketplace");
+			} else if (response.status !== 200) {
+				onSellItem("Error at listing the message");
+			}
+			// Update the user's inventory locally by reducing the quantity
+		} catch (error) {
+			console.error("Error selling item:", error);
+		}
+	};
 
-  return (
-    <form
-      className="relative border-2 border-yellow-600 w-2/4 flex flex-col text-light-text dark:text-dark-text"
-      onSubmit={handleSell}
-    >
-      <div className="flex justify-center items-center">
-        <p>{selectedItem?.itemName || "Select an item"}</p>
-      </div>
-      <div className="flex justify-center items-center">
-        <p>Quantity</p>
-        <button type="button">
-          <BiUpArrow
-            className=" text-xl"
-            onClick={() => {
-              if (quantity < selectedItem.quantity) setQuantity(quantity + 1);
-            }}
-          />
-        </button>
-        <input
-          type="text"
-          className="w-16 text-center"
-          maxLength={4}
-          min="1"
-          value={quantity}
-          onChange={onQuantityChange}
-        />
-        <button type="button">
-          <BiDownArrow
-            className="text-xl"
-            onClick={() => {
-              if (quantity > 1) setQuantity(quantity - 1);
-            }}
-          />
-        </button>
-      </div>
-      <div className="flex justify-center items-center">
-        <p>Price</p>
-        <input
-          type="text"
-          className="w-16 text-center"
-          min="1"
-          maxLength={6}
-          value={price}
-          onChange={onPriceChange}
-        />
-      </div>
-      <div className="flex items-center justify-center">
-        <button type="submit" className="border-2">
-          Sell
-        </button>
-      </div>
-    </form>
-  );
+	return (
+		<form
+			className="relative border-2 p-8 min-w-[350px] flex flex-col text-dark-text dark:text-light-text bg-dark-background dark:bg-light-background rounded-lg"
+			onSubmit={handleSell}
+		>
+			<div className="flex justify-center items-center p-4 text-3xl">
+				<p>{selectedItem?.itemName || "Select an item"}</p>
+			</div>
+			<div className="flex justify-center items-center p-2">
+				<p className="text-2xl">Quantity</p>
+				<button type="button">
+					<BiUpArrow
+						className=" text-xl"
+						onClick={() => {
+							if (quantity < selectedItem.quantity) setQuantity(quantity + 1);
+						}}
+					/>
+				</button>
+				<input
+					type="text"
+					className="w-16 text-center text-light-text dark:text-dark-text p-1 mx-1 rounded-md"
+					maxLength={4}
+					min="1"
+					value={quantity}
+					onChange={onQuantityChange}
+				/>
+				<button type="button">
+					<BiDownArrow
+						className="text-xl"
+						onClick={() => {
+							if (quantity > 1) setQuantity(quantity - 1);
+						}}
+					/>
+				</button>
+			</div>
+			<div className="flex justify-center items-center p-2 gap-2">
+				<p className="text-2xl">Price</p>
+				<input
+					type="text"
+					className="w-20 text-center text-light-text dark:text-dark-text p-1 rounded-md"
+					min="1"
+					maxLength={6}
+					value={price}
+					onChange={onPriceChange}
+				/>
+			</div>
+			<div className="flex items-center justify-center p-2 ">
+				<button
+					type="submit"
+					className="border-2 text-light-text dark:text-dark-text bg-light-background dark:bg-dark-background py-2 px-6 rounded-md"
+				>
+					Sell
+				</button>
+			</div>
+		</form>
+	);
 };
 
 export default SellItemDetail;
--- components/MarketPlace/SellTable.tsx
+++ components/MarketPlace/SellTable.tsx
@@ -10,54 +10,54 @@ import InventoryContainer from "../Inventory/InventoryContainer";
 import Message from "../Message";
 
 const SellTable = () => {
-  const { data: session } = useSession();
-  const [userInventory, setUserInventory] = useState([]);
-  const [selectedItem, setSelectedItem] = useState(null);
-  const [message, setMessage] = useState<string | null>(null);
+	const { data: session } = useSession();
+	const [userInventory, setUserInventory] = useState([]);
+	const [selectedItem, setSelectedItem] = useState(null);
+	const [message, setMessage] = useState<string | null>(null);
 
-  const handleSelectItem = (itemData: any) => {
-    // console.log("itemData en el hadnleselect", itemData);
-    setSelectedItem(itemData);
-  };
+	const handleSelectItem = (itemData: any) => {
+		// console.log("itemData en el hadnleselect", itemData);
+		setSelectedItem(itemData);
+	};
 
-  const updateMessage = (newMessage: string | null) => {
-    setMessage(newMessage);
-  };
-  const hideMessage = () => {
-    setMessage(null);
-  };
+	const updateMessage = (newMessage: string | null) => {
+		setMessage(newMessage);
+	};
+	const hideMessage = () => {
+		setMessage(null);
+	};
 
-  useEffect(() => {
-    const fetchInventory = async () => {
-      try {
-        // Make the API request with the user's ID
-        const response = await fetch(`/api/getinventory/${session?.user?.id}`);
-        const data = await response.json();
-        setUserInventory(data.items);
-      } catch (error) {
-        console.error("Error fetching inventory data:", error);
-      }
-    };
+	useEffect(() => {
+		const fetchInventory = async () => {
+			try {
+				// Make the API request with the user's ID
+				const response = await fetch(`/api/getinventory/${session?.user?.id}`);
+				const data = await response.json();
+				setUserInventory(data.items);
+			} catch (error) {
+				console.error("Error fetching inventory data:", error);
+			}
+		};
 
-    if (session?.user?.id) {
-      fetchInventory();
-    }
-  }, [session]);
-  return (
-    <div className="section flex justify-center items-center  ">
-      <InventoryContainer
-        items={userInventory}
-        onSelectItem={handleSelectItem} // Pass the handleSelectItem function
-      />
-      <SellItemDetail selectedItem={selectedItem} onSellItem={updateMessage} />
-      {message && (
-        <div className="absolute left-0 right-0 bottom-0 w-full  flex items-center  justify-center ">
-          <Message text={message} onClose={hideMessage} />
-        </div>
-      )}
-    </div>
-  );
-  // div con inventoryContainer/>  y  sellContainer flex
+		if (session?.user?.id) {
+			fetchInventory();
+		}
+	}, [session]);
+	return (
+		<div className="section flex justify-around items-center  flex-wrap">
+			<InventoryContainer
+				items={userInventory}
+				onSelectItem={handleSelectItem} // Pass the handleSelectItem function
+			/>
+			<SellItemDetail selectedItem={selectedItem} onSellItem={updateMessage} />
+			{message && (
+				<div className="absolute left-0 right-0 bottom-0 w-full  flex items-center  justify-center ">
+					<Message text={message} onClose={hideMessage} />
+				</div>
+			)}
+		</div>
+	);
+	// div con inventoryContainer/>  y  sellContainer flex
 };
 
 export default SellTable;
